b1797117cd47bc47d3299b4ac961274418f27ca8
shotaimai66 <changemymind6@gmail.com>
2019-08-27 19:26:15 +0900

    fix userのタスク一覧表示修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,9 +5,5 @@ module ApplicationHelper
     "#{date.year}-#{date.month}-#{date.day}T#{time.hour}:#{time.min}:00+09:00"
   end
   
-  def time_display(time)
-    "#{time.year}-#{time.month}-#{time.day} #{time.hour}時~#{time.hour + 1}時"
-    # "#{time.year}-#{time.month}-#{time.day} #{time.hour}~#{time.hour.1.hours.since}"
-  end
 
 end
