b3977a7115237b189c4c9c129d3bf2a8a44fe82e
shotaimai66 <changemymind6@gmail.com>
2019-09-08 15:15:39 +0900

    fix 時間修正

--- config/schedule.rb
+++ config/schedule.rb
@@ -32,7 +32,7 @@ if rails_env.to_sym != :development
         rake "task_reminder:push"
     end
 
-    every 1.day, :at => '3:10 pm' do
+    every 1.day, :at => '3:25 pm' do
         rake "task_reminder:test"
     end
 
