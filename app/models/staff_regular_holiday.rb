531861b4548040109fc2f41c45f995b876632189
shotaimai66 <changemymind6@gmail.com>
2019-09-09 15:05:56 +0900

    fix

--- app/models/staff_regular_holiday.rb
+++ app/models/staff_regular_holiday.rb
@@ -6,8 +6,10 @@ class StaffRegularHoliday < ApplicationRecord
 
     def update_staff_shifts
         self.staff.staff_shifts.where("work_date >= ?", Date.current).each do |shift|
+            start_time = shift.work_start_time.change(hour: (self.work_start_at).hour, min: self.work_start_at.min)
+            end_time = start_time.change(hour: (self.work_end_at).hour, min: self.work_end_at.min)
             if %w(日 月 火 水 木 金 土)[shift.work_date.wday] == self.day
-                shift.update(is_holiday: self.is_holiday, work_start_time: self.work_start_at, work_end_time: self.work_end_at)
+                shift.update(is_holiday: self.is_holiday, work_start_time: start_time, work_end_time: end_time)
             end
         end
     end
