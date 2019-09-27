383f6a90c7df8534c57855374bd31160b34d9e1f
shotaimai66 <changemymind6@gmail.com>
2019-09-27 12:46:56 +0900

    fix: db migration

--- db/migrate/20190924062531_add_index_to_staffs.rb
+++ db/migrate/20190924062531_add_index_to_staffs.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AddDeviseToStaffs < ActiveRecord::Migration[5.2]
+class AddIndexToStaffs < ActiveRecord::Migration[5.2]
   def change
     add_index :staffs, :email,                unique: true
     add_index :staffs, :reset_password_token, unique: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_24_052531) do
+ActiveRecord::Schema.define(version: 2019_09_24_062531) do
 
   create_table "admins", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
