6a50fe8f5586f8db1d7adbba860c03c69b53b318
shotaimai66 <changemymind6@gmail.com>
2019-09-29 23:52:12 +0900

    fix

--- app/controllers/staff/staff_base.rb
+++ app/controllers/staff/staff_base.rb
@@ -1,3 +1,11 @@
 class Staff::StaffBase < User::Base
   skip_before_action :authenticate_current_user!
+  before_action :authenticate_current_staff!
+
+  def authenticate_current_staff!
+    unless current_staff
+      flash[:danger] = "権限がありません"
+      redirect_to new_staff_session_url
+    end
+  end
 end
