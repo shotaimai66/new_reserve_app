674176ecd2123284cd28b53ffc7c900f6e87bc6e
shotaimai66 <changemymind6@gmail.com>
2019-10-13 12:20:45 +0900

    fix

--- app/models/task_course.rb
+++ app/models/task_course.rb
@@ -4,5 +4,5 @@ class TaskCourse < ApplicationRecord
   
   validates :title, presence: true
   validates :course_time, numericality: {greater_than: 0}
-  validates :charge, numericality: {greater_than: 0}
+  validates :charge, numericality: {greater_than_or_equal_to: 0}
 end
