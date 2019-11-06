286318dcca18a68c05730db953ae8f57e6baf2fc
shotaimai66 <changemymind6@gmail.com>
2019-11-07 08:15:54 +0900

    fix: ログイン時の挙動修正

--- app/controllers/staffs/sessions_controller.rb
+++ app/controllers/staffs/sessions_controller.rb
@@ -2,7 +2,7 @@
 
 class Staffs::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-
+  before_action :user_login?, onry: [:create]
   # GET /resource/sign_in
   # def new
   #   super
@@ -24,4 +24,10 @@ class Staffs::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  def user_login?
+    if current_user
+      sign_out(current_user)
+    end
+  end
 end
