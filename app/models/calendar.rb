914e457a26458ad95c7ed39ab053a359137da66e
shotaimai66 <changemymind6@gmail.com>
2019-11-10 13:35:53 +0900

    fix: 表示開始日修正

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -12,7 +12,7 @@ class Calendar < ApplicationRecord
   validates :display_interval_time, presence: true, numericality: { only_integer: true }
   validates :display_week_term, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 10 }
   validates :end_date, presence: true, numericality: { only_integer: true }
-  validates :start_date, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 10 }
+  validates :start_date, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
   validates :end_time, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 24 }
   validates :start_time, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 24 }
 
