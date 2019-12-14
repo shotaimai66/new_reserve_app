0e821d06cac693d3f04957b875d9a7704f324aa2
shotaimai66 <changemymind6@gmail.com>
2019-12-14 15:37:32 +0900

    fix

--- app/controllers/user/calendar_configs_controller.rb
+++ app/controllers/user/calendar_configs_controller.rb
@@ -5,6 +5,9 @@ class User::CalendarConfigsController < User::Base
     @calendar_config = Calendar.find_by(public_uid: params[:calendar_id]).calendar_config
     respond_to do |format|
       if @calendar_config.update(calendar_config_regular_holidays_params)
+        if params[:all_staff_update] == "true"
+          update_staff_regular_holiday
+        end
         format.html { redirect_to user_calendar_url(current_user, @calendar), notice: '詳細設定を更新しました。' }
       else
         format.html { redirect_to user_calendar_url(current_user, @calendar), notice: '詳細設定の更新に失敗しました。' }
@@ -15,6 +18,21 @@ class User::CalendarConfigsController < User::Base
   private
 
   def calendar_config_regular_holidays_params
-    params.require(:calendar_config).permit(:capacity, :interval_time, :booking_message, :booking_link, regular_holidays_attributes: %i[holiday_flag id business_start_at business_end_at is_reset rest_start_time rest_end_time])
+    params.require(:calendar_config).permit(:capacity, :interval_time, :booking_message, :booking_link, regular_holidays_attributes: %i[holiday_flag id business_start_at business_end_at is_rest rest_start_time rest_end_time])
   end
+
+  def update_staff_regular_holiday
+    @calendar_config.regular_holidays.each do |regular_holiday|
+      regular_holiday.staff_regular_holidays.each do |holiday|
+        holiday.update(is_holiday: regular_holiday.holiday_flag,
+                      work_start_at: regular_holiday.business_start_at,
+                      work_end_at: regular_holiday.business_end_at,
+                      rest_start_time: regular_holiday.rest_start_time,
+                      rest_end_time: regular_holiday.rest_end_time,
+                      is_rest: regular_holiday.is_rest,
+                      )
+      end
+    end
+  end
+
 end
--- app/models/regular_holiday.rb
+++ app/models/regular_holiday.rb
@@ -2,13 +2,17 @@ class RegularHoliday < ApplicationRecord
   belongs_to :calendar_config
   has_many :staff_regular_holidays
 
-  after_update :update_staff_regular_holiday
+  # after_update :update_staff_regular_holiday
 
-  def update_staff_regular_holiday
-    staff_regular_holidays.where(is_holiday: false).each do |holiday|
-      holiday.update(is_holiday: holiday_flag,
-                     work_start_at: business_start_at,
-                     work_end_at: business_end_at)
-    end
-  end
+  # def update_staff_regular_holiday
+  #   staff_regular_holidays.each do |holiday|
+  #     holiday.update(is_holiday: holiday_flag,
+  #                    work_start_at: business_start_at,
+  #                    work_end_at: business_end_at,
+  #                    rest_start_time: rest_start_time,
+  #                    rest_end_time: rest_end_time,
+  #                    is_rest: is_rest,
+  #                   )
+  #   end
+  # end
 end
