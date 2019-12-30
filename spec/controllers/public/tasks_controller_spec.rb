00eaab54d48732adfb283bd9d17ca9c1708101da
shotaimai66 <changemymind6@gmail.com>
2019-12-30 13:55:11 +0900

    fix: action_cableの設定

--- spec/controllers/public/tasks_controller_spec.rb
+++ spec/controllers/public/tasks_controller_spec.rb
@@ -44,10 +44,10 @@ RSpec.describe Public::TasksController, type: :controller do
                     get :new, params: {calendar_id: @calendar.public_uid, staff_id: @staff, course_id: @course, start_time: Time.current.days_ago(2)}
                 end
                 it 'リクエストは200 OKとなること' do
-                    expect(response.status).to eq 500
+                    expect(response.status).to eq 302
                 end
                 it ':newテンプレートを表示すること' do
-                    expect(response).to render_template "errors/error_500"
+                    expect(response).to redirect_to calendar_tasks_url(@calendar)
                 end
             end
         end
