6b746758876fb286989e90c102fbfc2c783dd6de
shotaimai66 <changemymind6@gmail.com>
2019-12-31 09:05:01 +0900

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,9 +43,9 @@ Rails.application.configure do
 
 
   # Mount Action Cable outside main process or domain
-  # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  config.action_cable.allowed_request_origins = [ /https?:\/\/.*/ ]
+  config.action_cable.mount_path = '/cable'
+  config.action_cable.url = 'wss://booking-env-action-cable.ap-northeast-1.elasticbeanstalk.com/cable'
+  config.action_cable.allowed_request_origins = [ 'https://booking-env-action-cable.ap-northeast-1.elasticbeanstalk.com/', /https:\/\/booking-env-action-cable.ap-northeast-1.elasticbeanstalk.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
