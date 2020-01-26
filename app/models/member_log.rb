9a0da32f1518d47ef6385c61a4e533ed2b29a146
shotaimai66 <changemymind6@gmail.com>
2020-01-26 21:02:11 +0900

    fix

--- app/models/member_log.rb
+++ app/models/member_log.rb
@@ -1,5 +1,5 @@
 class MemberLog < ApplicationRecord
   belongs_to :store_member
   has_many :log_pictures, dependent: :destroy
-  # accepts_nested_attributes_for :log_pictures, allow_destroy: true
+  accepts_nested_attributes_for :log_pictures, allow_destroy: true
 end
