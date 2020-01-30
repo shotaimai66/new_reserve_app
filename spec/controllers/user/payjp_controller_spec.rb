3b436ecb8fd24cd2a1536eea91f3d72ec93b99e8
shotaimai66 <changemymind6@gmail.com>
2020-01-30 20:28:04 +0900

    fix

--- spec/controllers/user/payjp_controller_spec.rb
+++ spec/controllers/user/payjp_controller_spec.rb
@@ -1,4 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User::PayjpController, type: :controller do
+  describe '' do
+    # before do
+    #   @user = create(:user)
+    #   @calendar = create(:calendar, user: @user)
+    #   @staff = create(:staff, calendar: @calendar)
+    #   @calendar.update(is_released: true)
+    #   @course = @calendar.task_courses.first
+    # end
+
+    describe 'Get #privacy' do
+      before do
+        get :privacy
+      end
+      it 'リクエストは200 OKとなること' do
+        expect(response.status).to eq 200
+      end
+      it ':indexテンプレートを表示すること' do
+        expect(response).to render_template :privacy
+      end
+    end
+  end
 end
