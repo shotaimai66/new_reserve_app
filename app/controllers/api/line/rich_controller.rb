09d8f3edd595d72c793c2b2136f920bf7170951f
shotaimai66 <changemymind6@gmail.com>
2020-02-21 11:30:36 +0900

    fix:環境変数

--- app/controllers/api/line/rich_controller.rb
+++ app/controllers/api/line/rich_controller.rb
@@ -37,8 +37,8 @@ private
 # LINE Developers登録完了後に作成される環境変数の認証
   def client
     @client ||= Line::Bot::Client.new { |config|
-      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+      config.channel_secret = ENV["CHANNEL_SECRET"]
+      config.channel_token = ENV["CHANNEL_TOKEN"]
     }
   end
 
