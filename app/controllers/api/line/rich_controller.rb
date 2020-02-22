75df7643066650ecfbf4b0eb61f887c797dc1942
shotaimai66 <changemymind6@gmail.com>
2020-02-22 11:58:55 +0900

    fix

--- app/controllers/api/line/rich_controller.rb
+++ app/controllers/api/line/rich_controller.rb
@@ -10,17 +10,22 @@ class Api::Line::RichController < ApplicationController
       error 400 do 'Bad Request' end
     end
     events = client.parse_events_from(body)
-
+    line_user_id = params['events'][0]['source']['userId']
+    store_member = StoreMember.find(line_user_id: line_user_id)
+    tasks = store_member.tasks.where("start_time > ?", Time.current)
     events.each do |event|
       case event
       when Line::Bot::Event::Message
         case event.type
         when Line::Bot::Event::MessageType::Text
           if event.message['text'] == "予約確認"
-            message = {
-              type: 'text',
-              text: "予約確認ですねー！お待ちください！"
-            }
+            tasks.each do |task|
+              message = {
+                type: 'text',
+                text: task_message(task)
+              }
+              client.reply_message(event['replyToken'], message)
+            end
           else
             message = {
               type: 'text',
@@ -43,6 +48,20 @@ private
       config.channel_secret = ENV["CHANNEL_SECRET"]
       config.channel_token = ENV["CHANNEL_TOKEN"]
     }
+  end
+
+  def task_message(task)
+    str2 = <<-EOS
+      ・予約時間
+      #{l(task.start_time, format: :middle)}~
+      #{l(task.end_time, format: :middle)}
+      ・予約コース
+      #{task.task_course.title}
+      ・コース料金
+      #{task.task_course.charge}
+      ・担当スタッフ
+      #{task.staff_name}
+    EOS
   end
 
   
