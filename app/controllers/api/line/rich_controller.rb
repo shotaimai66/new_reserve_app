993eb579afc29884eaaae986b5104f6add85d0da
shotaimai66 <changemymind6@gmail.com>
2020-02-23 13:24:10 +0900

    fix

--- app/controllers/api/line/rich_controller.rb
+++ app/controllers/api/line/rich_controller.rb
@@ -21,70 +21,153 @@ class Api::Line::RichController < ApplicationController
           if event.message['text'] == "予約確認"
             tasks.each do |task|
               message = {
-                "type": "template",
-                "altText": "this is a carousel template",
-                "template": {
-                    "type": "carousel",
-                    "columns": [
+                "type": "bubble",
+                "hero": {
+                  "type": "image",
+                  "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
+                  "size": "full",
+                  "aspectRatio": "20:13",
+                  "aspectMode": "cover",
+                  "action": {
+                    "type": "uri",
+                    "uri": "http://linecorp.com/"
+                  }
+                },
+                "body": {
+                  "type": "box",
+                  "layout": "vertical",
+                  "contents": [
+                    {
+                      "type": "text",
+                      "text": "Brown Cafe",
+                      "weight": "bold",
+                      "size": "xl"
+                    },
+                    {
+                      "type": "box",
+                      "layout": "baseline",
+                      "margin": "md",
+                      "contents": [
                         {
-                          "thumbnailImageUrl": "https://example.com/bot/images/item1.jpg",
-                          "imageBackgroundColor": "#FFFFFF",
-                          "title": "this is menu",
-                          "text": "description",
-                          "defaultAction": {
-                              "type": "uri",
-                              "label": "View detail",
-                              "uri": "http://example.com/page/123"
-                          },
-                          "actions": [
-                              {
-                                  "type": "postback",
-                                  "label": "Buy",
-                                  "data": "action=buy&itemid=111"
-                              },
-                              {
-                                  "type": "postback",
-                                  "label": "Add to cart",
-                                  "data": "action=add&itemid=111"
-                              },
-                              {
-                                  "type": "uri",
-                                  "label": "View detail",
-                                  "uri": "http://example.com/page/111"
-                              }
+                          "type": "icon",
+                          "size": "sm",
+                          "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+                        },
+                        {
+                          "type": "icon",
+                          "size": "sm",
+                          "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+                        },
+                        {
+                          "type": "icon",
+                          "size": "sm",
+                          "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+                        },
+                        {
+                          "type": "icon",
+                          "size": "sm",
+                          "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+                        },
+                        {
+                          "type": "icon",
+                          "size": "sm",
+                          "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png"
+                        },
+                        {
+                          "type": "text",
+                          "text": "4.0",
+                          "size": "sm",
+                          "color": "#999999",
+                          "margin": "md",
+                          "flex": 0
+                        }
+                      ]
+                    },
+                    {
+                      "type": "box",
+                      "layout": "vertical",
+                      "margin": "lg",
+                      "spacing": "sm",
+                      "contents": [
+                        {
+                          "type": "box",
+                          "layout": "baseline",
+                          "spacing": "sm",
+                          "contents": [
+                            {
+                              "type": "text",
+                              "text": "Place",
+                              "color": "#aaaaaa",
+                              "size": "sm",
+                              "flex": 1
+                            },
+                            {
+                              "type": "text",
+                              "text": "Miraina Tower, 4-1-6 Shinjuku, Tokyo",
+                              "wrap": true,
+                              "color": "#666666",
+                              "size": "sm",
+                              "flex": 5
+                            }
                           ]
                         },
                         {
-                          "thumbnailImageUrl": "https://example.com/bot/images/item2.jpg",
-                          "imageBackgroundColor": "#000000",
-                          "title": "this is menu",
-                          "text": "description",
-                          "defaultAction": {
-                              "type": "uri",
-                              "label": "View detail",
-                              "uri": "http://example.com/page/222"
-                          },
-                          "actions": [
-                              {
-                                  "type": "postback",
-                                  "label": "Buy",
-                                  "data": "action=buy&itemid=222"
-                              },
-                              {
-                                  "type": "postback",
-                                  "label": "Add to cart",
-                                  "data": "action=add&itemid=222"
-                              },
-                              {
-                                  "type": "uri",
-                                  "label": "View detail",
-                                  "uri": "http://example.com/page/222"
-                              }
+                          "type": "box",
+                          "layout": "baseline",
+                          "spacing": "sm",
+                          "contents": [
+                            {
+                              "type": "text",
+                              "text": "Time",
+                              "color": "#aaaaaa",
+                              "size": "sm",
+                              "flex": 1
+                            },
+                            {
+                              "type": "text",
+                              "text": "10:00 - 23:00",
+                              "wrap": true,
+                              "color": "#666666",
+                              "size": "sm",
+                              "flex": 5
+                            }
                           ]
                         }
-                    ],
-                    "imageAspectRatio": "rectangle",
-                    "imageSize": "cover"
+                      ]
+                    }
+                  ]
+                },
+                "footer": {
+                  "type": "box",
+                  "layout": "vertical",
+                  "spacing": "sm",
+                  "contents": [
+                    {
+                      "type": "button",
+                      "style": "link",
+                      "height": "sm",
+                      "action": {
+                        "type": "uri",
+                        "label": "CALL",
+                        "uri": "https://linecorp.com"
+                      }
+                    },
+                    {
+                      "type": "button",
+                      "style": "link",
+                      "height": "sm",
+                      "action": {
+                        "type": "uri",
+                        "label": "WEBSITE",
+                        "uri": "https://linecorp.com"
+                      }
+                    },
+                    {
+                      "type": "spacer",
+                      "size": "sm"
+                    }
+                  ],
+                  "flex": 0
                 }
               }
               response = client.reply_message(event['replyToken'], message)
